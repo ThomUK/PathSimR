1dd8e39caf8fb5ccaba79b8c416574d4b16cf5a7
Tom Smith <thomas.smith103@nhs.net>
2026-03-10 14:36:23 +0000

    calculate processor cores required

---
+++ R/processor_cores_required.R
@@ -0,0 +1,17 @@
+#' Calculate the number of cores to use, which is limited by both the number
+#' available, and the number of replications required
+#'
+#' @param replications integer. Number of simulation replications required
+#' @param cores integer. Number of processor cores available
+#'
+#' @returns integer. The number of cores to use (1 minimum)
+#'
+processor_cores_required <- function(replications, cores = parallel::detectCores()){
+
+  limit_to_replications <- max(replications, 1)
+
+  limit_to_cores <- min(cores - 1, replications)
+
+  max(limit_to_cores, 1) # we need at least one core!
+
+}
