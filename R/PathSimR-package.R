966d1864d9fbdc086e570c514455881f1365ceef
Tom Smith <ThomUK@users.noreply.github.com>
2026-03-13 12:40:23 +0000

    add package-side imports to cover code outside of the parallel loop

--- R/PathSimR-package.R
+++ R/PathSimR-package.R
@@ -2,6 +2,10 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom dplyr group_by
+#' @importFrom dplyr mutate
+#' @importFrom dplyr summarise
+#' @importFrom dplyr transmute
 #' @importFrom magrittr %>%
 #' @importFrom shiny req
 ## usethis namespace: end
