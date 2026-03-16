b4b1a13dcc05e4b4d764e57a8039b32f3416cbb6
Tom Smith <ThomUK@users.noreply.github.com>
2026-03-16 17:00:56 +0000

    Revert "convert tidyr::spread to tidyr::pivot_wider"
    This reverts commit 456ad2bd5393e23632710757cce0bc7c070e795e.

--- R/run_single_replication.R
+++ R/run_single_replication.R
@@ -4879,10 +4879,10 @@ run_single_replication <- function(j, var_input, syst_names, syst_names_single,
 
 
   # tidyr:: namespace required inside parallel operations
-  multi_spread <- tidyr::pivot_wider(
+  multi_spread <- tidyr::spread(
     data = multi,
-    names_from = metric,
-    values_from = value
+    key = metric,
+    value = value
   )
 
 
